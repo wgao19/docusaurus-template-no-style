2b0948db734750cb0115a7b917eb5b7220d125e8
Wei Gao <wgao19@gmail.com>
2019-07-28 04:54:14 +0000

    remove links from footer

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -5,58 +5,26 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import withBaseUrl from '@docusaurus/withBaseUrl';
-import styles from './styles.module.css';
+import React from "react";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import withBaseUrl from "@docusaurus/withBaseUrl";
+import styles from "./styles.module.css";
 
 function Footer() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
-  const {themeConfig = {}} = siteConfig;
-  const {footer} = themeConfig;
+  const { siteConfig = {} } = context;
+  const { themeConfig = {} } = siteConfig;
+  const { footer } = themeConfig;
 
   if (!footer) {
     return null;
   }
 
-  const {copyright, links = [], logo} = footer;
+  const { copyright, links = [], logo } = footer;
 
   return (
     <footer>
-      {links && links.length > 0 && (
-        <section>
-          {links.map((linkItem, i) => (
-            <div key={i} className={styles.linkSection}>
-              {linkItem.title != null ? <h4>{linkItem.title}</h4> : null}
-              {linkItem.items != null &&
-              Array.isArray(linkItem.items) &&
-              linkItem.items.length > 0 ? (
-                <ul>
-                  {linkItem.items.map(item => (
-                    <li key={item.href || item.to}>
-                      <Link
-                        {...item}
-                        {...(item.href
-                          ? {
-                              target: '_blank',
-                              rel: 'noopener noreferrer',
-                              href: item.href,
-                            }
-                          : {
-                              to: withBaseUrl(item.to),
-                            })}>
-                        {item.label}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              ) : null}
-            </div>
-          ))}
-        </section>
-      )}
       {(logo || copyright) && (
         <section className={styles.copyright}>
           {logo && logo.src && <img alt={logo.alt} src={logo.src} />}
